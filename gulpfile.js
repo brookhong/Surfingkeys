c2fdd46ba2c438b8aaee54e12036fb882bcceb60
brook hong <hzgmaxwell@hotmail.com>
2016-09-08 18:51:20 +0800

    0.6.1

--- gulpfile.js
+++ gulpfile.js
@@ -44,11 +44,11 @@ gulp.task('use_common_content_min', ['copy-non-js-files', 'clean'], function() {
         'pages/github-markdown.html'
     ], {base: "."})
         .pipe(replace(/.*build:common_content[^]*endbuild.*/, '        <script src="../content_scripts/common_content.min.js"></script>'))
-        .pipe(replace('sha256-jNQvOEIBE16jqoFcbLrv8oLxcfm2/tznTOaOlxs+skM=', 'sha256-i4N/bue6A5u2Mc9Hlv8sfqDlQcHZL6PylEhdkrXtPbU='))
+        .pipe(replace('sha256-jNQvOEIBE16jqoFcbLrv8oLxcfm2/tznTOaOlxs+skM=', 'sha256-zTMMm8Fvw6JyLuDfGzLA/fvSxrCYADLD+7zrn4NBDh8='))
         .pipe(gulp.dest('dist'));
     gulp.src('manifest.json')
         .pipe(replace(/.*build:common_content[^]*endbuild.*/, '            "content_scripts/common_content.min.js",'))
-        .pipe(replace('sha256-jNQvOEIBE16jqoFcbLrv8oLxcfm2/tznTOaOlxs+skM=', 'sha256-i4N/bue6A5u2Mc9Hlv8sfqDlQcHZL6PylEhdkrXtPbU='))
+        .pipe(replace('sha256-jNQvOEIBE16jqoFcbLrv8oLxcfm2/tznTOaOlxs+skM=', 'sha256-zTMMm8Fvw6JyLuDfGzLA/fvSxrCYADLD+7zrn4NBDh8='))
         .pipe(gulp.dest('dist'));
 });
 
