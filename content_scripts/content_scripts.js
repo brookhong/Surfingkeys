8e4d2309f867c9216c15f463cbb97eeaaacbd82d
brook hong <hzgmaxwell@hotmail.com>
2017-02-11 12:22:23 +0800

    Fixed #253 Zoom does not respect numeric prefix

--- content_scripts/content_scripts.js
+++ content_scripts/content_scripts.js
@@ -40,7 +40,7 @@ function parseCommand(cmdline) {
 }
 
 RUNTIME = function(action, args) {
-    var actionsRepeatBackground = ['closeTab', 'nextTab', 'previousTab', 'moveTab', 'reloadTab'];
+    var actionsRepeatBackground = ['closeTab', 'nextTab', 'previousTab', 'moveTab', 'reloadTab', 'setZoom'];
     (args = args || {}).action = action;
     if (actionsRepeatBackground.indexOf(action) !== -1) {
         // if the action can only be repeated in background, pass repeats to background with args,
