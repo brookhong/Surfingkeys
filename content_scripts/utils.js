d5dfa56e87658cf471c48902002e1a4d120dfb60
Simon W. Jackson <simon@simonwjackson.com>
2017-10-15 12:19:52 -0500

    move `parseQueryString` to `Utils`

--- content_scripts/utils.js
+++ content_scripts/utils.js
@@ -43,6 +43,20 @@ const Utils = (function () {
             || (element.localName === 'input' && /^(?!button|checkbox|file|hidden|image|radio|reset|submit)/i.test(element.type));
     }
 
+    function parseQueryString(query) {
+        var params = {};
+        if (query.length) {
+            var parts = query.split('&');
+            for (var i = 0, ii = parts.length; i < ii; ++i) {
+                var param = parts[i].split('=');
+                var key = param[0].toLowerCase();
+                var value = param.length > 1 ? param[1] : null;
+                params[decodeURIComponent(key)] = decodeURIComponent(value);
+            }
+        }
+        return params;
+    }
+
     return {
         getDocumentOrigin,
         timeStampString,
@@ -50,23 +64,12 @@ const Utils = (function () {
         htmlEncode,
         htmlDecode,
         getRealEdit,
-        isEditable
+        isEditable,
+        parseQueryString
     };
 })();
 
-function parseQueryString(query) {
-    var params = {};
-    if (query.length) {
-        var parts = query.split('&');
-        for (var i = 0, ii = parts.length; i < ii; ++i) {
-            var param = parts[i].split('=');
-            var key = param[0].toLowerCase();
-            var value = param.length > 1 ? param[1] : null;
-            params[decodeURIComponent(key)] = decodeURIComponent(value);
-        }
-    }
-    return params;
-}
+
 
 function reportIssue(title, description) {
     title = encodeURIComponent(title);
