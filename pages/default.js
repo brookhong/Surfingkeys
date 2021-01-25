1de4ef7ee9d07d06f6fb3676cb4e82dc291947f0
Cibin Mathew <10793628+cibinmathew@users.noreply.github.com>
2021-01-25 15:27:33 +0530

    Update default.js

--- pages/default.js
+++ pages/default.js
@@ -418,8 +418,8 @@ mapkey('yy', "#7Copy current page's URL", function() {
 });
 mapkey('yY', "#7Copy all tabs's url", function() {
     RUNTIME('getTabs', null, function (response) {
-        Clipboard.write([window.location.href].concat(response.tabs.map(tab => tab.url)).join('\n'))
-    })
+        Clipboard.write([window.location.href].concat(response.tabs.map(tab => tab.url)).join('\n'));
+    });
 });    
 mapkey('yh', "#7Copy current page's host", function() {
     var url = new URL(window.location.href);
