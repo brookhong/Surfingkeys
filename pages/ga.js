3f13acff79f7bed77beb888d41fef2d5f3247e8e
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-14 13:18:18 +0800

    Delete pages directory

--- pages/ga.js
+++
@@ -1,6 +0,0 @@
-window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments);};ga.l=+new Date;
-ga('create', 'UA-108751049-1', {'siteSpeedSampleRate': 100});
-ga('set', 'checkProtocolTask', function(){});
-ga('set', 'location', 'http://surfingkeys.com/'+document.location.pathname);
-ga('set', 'dimension1', chrome.runtime.id);
-ga('send', 'pageview');
