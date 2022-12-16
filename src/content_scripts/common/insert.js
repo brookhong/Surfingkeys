671df70744b3d0beca86a3a14d2f1ed8387c80ef
colossatr0n <29556317+colossatr0n@users.noreply.github.com>
2022-12-15 23:40:31 -0700

    Fix cursor not moving to EOL for CodeMirror elements.

--- src/content_scripts/common/insert.js
+++ src/content_scripts/common/insert.js
@@ -32,12 +32,38 @@ function createInsert() {
                 if (node.nodeType === Node.TEXT_NODE) {
                     document.getSelection().setPosition(node, node.data.length);
                 } else {
-                    document.getSelection().setPosition(node, node.childNodes.length);
+                    let codeMirrorNode = node.querySelector(".CodeMirror-line")
+                    if (codeMirrorNode) {
+                        setEndOfContenteditable(element)
+                    } else {
+                        document.getSelection().setPosition(node, node.childNodes.length);
+                    }
                 }
                 // blink cursor to bring cursor into view
                 Visual.showCursor();
                 Visual.hideCursor();
             }
+        }
+    }
+
+    // From https://stackoverflow.com/questions/1125292/how-to-move-cursor-to-end-of-contenteditable-entity/69727327#69727327
+    function setEndOfContenteditable(contentEditableElement) {
+        var range, selection;
+        if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+        {
+            range = document.createRange();//Create a range (a range is a like the selection but invisible)
+            range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
+            range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+            selection = window.getSelection();//get the selection object (allows you to change selection)
+            selection.removeAllRanges();//remove any selections already made
+            selection.addRange(range);//make the range you have just created the visible selection
+        }
+        else if(document.selection)//IE 8 and lower
+        {
+            range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+            range.moveToElementText(contentEditableElement);//Select the entire contents of the element with the range
+            range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+            range.select();//Select the range (make it the visible selection)
         }
     }
 
