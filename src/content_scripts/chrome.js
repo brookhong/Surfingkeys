c46f9890340037c8fce2c38b9a9e00c3e8a38eed
twio142 <64556708+twio142@users.noreply.github.com>
2025-12-26 04:41:56 +0100

    fix(pdf): viewer URL not handling multiple search parameters

--- src/content_scripts/chrome.js
+++ src/content_scripts/chrome.js
@@ -5,7 +5,7 @@ import { dispatchSKEvent, runtime, RUNTIME } from './common/runtime.js';
 import { start } from './content.js';
 
 function usePdfViewer() {
-    window.location.replace(chrome.runtime.getURL("/pages/pdf_viewer.html") + "?file=" + document.URL);
+    window.location.replace(chrome.runtime.getURL("/pages/pdf_viewer.html") + "?file=" + encodeURIComponent(document.URL));
 }
 
 function readText(text, options) {
